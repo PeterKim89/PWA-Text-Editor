99ebbb800ab404ab2b08c9eb36fb6b99aa840560
Peter Kim <peter.kim@uconn.edu>
2022-12-01 23:27:01 -0500

    added icons to manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,20 +27,27 @@ module.exports = () => {
 				swSrc: "./src-sw.js",
 				swDest: "src-sw.js",
 			}),
+
+			new webPackManifest({
+				fingerprints: false,
+				inject: true,
+				name: "Text Editor",
+				short_name: "Editor",
+				description: "JATE Text Editor",
+				background_color: "#225ca3",
+				theme_color: "#225ca3",
+				start_url: "./",
+				publicPath: "./",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join("assets", "icons"),
+					},
+				],
+			}),
 		],
 
-    new webPackManifest({
-      fingerprints: false,
-      inject: true,
-      name: "Text Editor",
-      short_name: "Editor",
-      description: "JATE Text Editor",
-      background_color: "#225ca3",
-      theme_color: "#225ca3",
-      start_url: './',
-      publicPath: './',
-      icons: []
-    }),
 		module: {
 			rules: [],
 		},
